7e92100f2906e1c28246f9ca4fbc92247c5d76e3
vanuan <vanuan@gmail.com>
2016-02-21 22:38:18 +0200

    Full fitbounds, add more links

--- odessa.js
+++ odessa.js
@@ -1,6 +1,6 @@
 /* global L */
 /* global $ */
-var map = L.map('map');
+var map = L.map('map', {zoomSnap: 0});
 var myLayer = L.geoJson().addTo(map);
 
 $.getJSON("export.geojson", {
@@ -8,7 +8,7 @@ $.getJSON("export.geojson", {
     "type": "boundary"
   }).done(function(json) {
     json.features = json.features.filter(function(feature, layer) {
-      if (feature.geometry.type == "Point") {
+      if (['Point', 'LineString'].indexOf(feature.geometry.type) != -1) {
         return false;
       }
       else {
@@ -16,27 +16,40 @@ $.getJSON("export.geojson", {
       }
     });
     var htmlProperties = function(properties) {
-      var fieldsToShow = ['name', 'wikipedia', 'koatuu'];
-
+      var fieldsToShow = ['name', 'wikipedia', 'wikidata', 'koatuu'];
+      var html = '';
+      html += '<h3>' + properties['name'] + '</h3>';
+      html += '<a target="_blank" href="http://data-gov-ua.org/dataset/koatuu">КОАТУУ</a> ' + properties['koatuu'] + '<br/>';
+      html += '<a target="_blank" href="http://www.openstreetmap.org/' + properties['@id'] + '">OpenStreetMap</a>' + '<br/>';
+      html += '<a target="_blank" href="https://www.wikipedia.org/wiki/' + properties['wikipedia'] + '">Вікіпедія</a>' + '<br/>';
+      html += '<a target="_blank" href="https://www.wikidata.org/wiki/' + properties['wikidata'] + '">Вікідата</a>' + '<br/>';
+      return html;
       return Object.keys(properties).filter(function(propertyName) {
         return fieldsToShow.indexOf(propertyName) != -1;
       }).map(function(propertyName) {
-        if (propertyName == 'wikipedia') {
-          return '<a target="_blank" href="https://www.wikipedia.org/wiki/' + properties[propertyName] + '"> wikipedia <a>';
+        var html;
+        if (propertyName == 'name') {
+          html = '<h1>' + properties[propertyName] + '</h1>'
         }
-        return propertyName + ': ' + properties[propertyName];
+        if (propertyName == 'wikipedia') {
+          html = '<a target="_blank" href="https://www.wikipedia.org/wiki/' + properties[propertyName] + '">Вікіпедія</a>';
+        } else if (propertyName == 'wikidata') {
+          html = '<a target="_blank" href="https://www.wikidata.org/wiki/' + properties[propertyName] + '">Вікідата</a>';
+        } else if (propertyName == 'koatuu') {
+        } else {
+          html = propertyName + ': ' + properties[propertyName];
+        }
+        return html;
       }).join('<br/>');
     };
 
     var geojson = L.geoJson(json, {
       onEachFeature: function(feature, layer) {
-          var popupHtmlContent = htmlProperties(feature.properties);
-          layer.bindPopup(popupHtmlContent);
+        var popupHtmlContent = htmlProperties(feature.properties);
+        layer.bindPopup(popupHtmlContent);
       }
     });
     geojson.addTo(map);
-
-
     myLayer.addData(json);
     map.fitBounds(myLayer.getBounds());
   })

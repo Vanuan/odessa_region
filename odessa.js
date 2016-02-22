a228c4a18720caba384cc91037a27c9cec8b20aa
vanuan <vanuan@gmail.com>
2016-02-22 06:36:16 +0200

    Add data download script

--- odessa.js
+++ odessa.js
@@ -20,7 +20,7 @@ $.getJSON("export.geojson", {
       var html = '';
       html += '<h3>' + properties['name'] + '</h3>';
       html += '<a target="_blank" href="http://data-gov-ua.org/dataset/koatuu">КОАТУУ</a> ' + properties['koatuu'] + '<br/>';
-      html += '<a target="_blank" href="http://www.openstreetmap.org/' + properties['@id'] + '">OpenStreetMap</a>' + '<br/>';
+      html += '<a target="_blank" href="http://www.openstreetmap.org/' + properties['id'] + '">OpenStreetMap</a>' + '<br/>';
       html += '<a target="_blank" href="https://www.wikipedia.org/wiki/' + properties['wikipedia'] + '">Вікіпедія</a>' + '<br/>';
       html += '<a target="_blank" href="https://www.wikidata.org/wiki/' + properties['wikidata'] + '">Вікідата</a>' + '<br/>';
       return html;
